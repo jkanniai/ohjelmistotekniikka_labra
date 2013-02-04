130475d05243e24071a820fead05ea7d8ac98670
Janne Kanniainen <janne.kanniainen@gmail.com>
2013-02-04 19:06:58 +0200

    Something something

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main()
   std::string firstname,surname;
   std::cout << "What is your name?" << std::endl;
   std::cin >> firstname >> surname;
-  std::cout << "Hi! Your name is " << firstname << " " << surname << " maybe?" << std::endl;
+  std::cout << "Hi! Your name is " << firstname << " " << surname << " maybe?" << std::endl << std::endl;
   return 0;
 }
