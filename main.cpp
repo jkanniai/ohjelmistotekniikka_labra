20c0fb3f8d31906599cde73ce9cae9da11212927
Janne Kanniainen <janne.kanniainen@gmail.com>
2013-02-04 19:11:37 +0200

    All unnecessary things deleted

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <string>
 
 int main()
 {
-  int i = 0;
-  std::string firstname,surname;
-  std::cout << "What is your name?" << std::endl;
-  std::cin >> firstname >> surname;
-  std::cout << "Hi! Your name is " << firstname << " " << surname << " maybe?" << std::endl << std::endl;
+  std::cout << "Hello, World!" << std::endl;
+
   return 0;
 }
